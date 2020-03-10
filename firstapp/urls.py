bd5fa38e9f4a26700255796bb354d772a5adf377
GouthamDoddi <gouthambolt@gmail.com>
2020-03-10 17:28:52 +0530

    blogsite1

---
+++ firstapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
+urlpatterns = [
+    path('', views.home, name='guitar-lovers-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('forums/', PostListView.as_view(), name='guitar-lovers-fourms'),
+    path('forums/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('forums/new/', PostCreateView.as_view(), name='post-new'),
+    path('forums/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('forums/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+
+]
