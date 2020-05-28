f0ad7f1bfdcf750ef4bb4fd0f1b8c3e811bd7f88
GouthamDoddi <gouthambolt@gmail.com>
2020-05-28 15:07:48 +0530

    changed whitenoice config

--- not_first_project/settings.py
+++ not_first_project/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'live-static-files', 'static-root')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
