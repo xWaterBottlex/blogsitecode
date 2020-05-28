e04151d6523a5b7213bb89735f9cc4d94b85eb2c
GouthamDoddi <gouthambolt@gmail.com>
2020-05-28 15:25:59 +0530

    Added gitignore

--- not_first_project/settings.py
+++ not_first_project/settings.py
@@ -126,11 +126,11 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'live-static-files', 'static-root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'live-static', 'static-root')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'live-static', 'media')
 MEDIA_URL = '/media/'
 
 
