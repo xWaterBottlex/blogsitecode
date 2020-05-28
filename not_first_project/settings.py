306e5aa6ebad05df9854c7b9ce9e26ea4283832a
GouthamDoddi <gouthambolt@gmail.com>
2020-05-28 15:03:37 +0530

    added static files for tracking

--- not_first_project/settings.py
+++ not_first_project/settings.py
@@ -124,6 +124,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'live-static-files', 'static-root')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
