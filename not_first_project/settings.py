aae30a6af91635ffe0967e6dae4836d69fea28da
GouthamDoddi <gouthambolt@gmail.com>
2020-05-28 17:26:33 +0530

    Added missing jquery cdn

--- not_first_project/settings.py
+++ not_first_project/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 MEDIA_URL = '/media/'
 
 
